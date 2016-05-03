af89a35bc4dda5d86e1b45008eff764cac902ec4
Clarke Benedict Plumo <clarke.plumo@gmail.com>
2016-05-03 09:14:21 +0800

    Convert date field to date time

--- tasks/models.py
+++ tasks/models.py
@@ -7,8 +7,8 @@ class Task(models.Model):
     title = models.CharField(max_length=255)
     body = models.TextField()
     assigned_to = models.ForeignKey(User)
-    created_at = models.DateField(auto_now_add=True)
-    updated_at = models.DateField(auto_now=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return "[%s] %s" % (self.assigned_to, self.title)
